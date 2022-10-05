7d01d618f7dab1a09800aec53d12d519ddd8a89b
Acuao <acuao@live.fr>
2022-10-06 01:18:37 +0200

    passage lib en projet typescript

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,7 @@ const base64Data= process.argv[3];
 // function to create file from base64 encoded string
 function base64ToFile(base64str: string, file: string) {
     // create buffer object from base64 encoded string, it is important to tell the constructor that the string is base64 encoded
-    var buffer = new Buffer(base64str, 'base64');
+    var buffer = Buffer.from(base64str, 'base64');
     // write buffer to file
     fs.writeFileSync(file, buffer);
 }
