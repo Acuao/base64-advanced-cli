9a72e345ba4731a0578a7fd7e14d4b75c84f70f6
Acuao <acuao@live.fr>
2022-10-21 18:08:44 +0200

    Release v1.0.0

--- readme.md
+++ readme.md
@@ -1,5 +1,5 @@
 # base64-advanced-cli
-This command line interface helps you to encode/decode base64 data with advanced features like files encode/decode
+This command line interface helps you to **encode/decode base64** data with advanced features like **files encoding/decoding**
 
 ## Install
 
@@ -13,14 +13,43 @@ $ npm install --global base64-advanced-cli
 This CLI provides the following command : `b64`
 
 ```
-$ b64 [outputFileName] [base64EncodedData]
+Usage: b64 [options]
+
+Options:
+  -d, --decode [data]
+  -e, --encode [data]
+  -i, --input-file <filename>
+  -o, --output-file <filename>
+  -h, --help                    display help for command
 ```
 
-**Example :**
+### Usage examples :
+
+**Encoding:**
 ```
-$ b64 decoded.txt SGVsbG8gd29ybGQh
+$ b64 -e "Hello World!"
 ```
-Will create a file named `decoded.txt` containing text " *Hello world!* "
+Will return `SGVsbG8gV29ybGQh` value.
+
+**Decoding:**
+
+```
+$ b64 -d SGVsbG8gV29ybGQh
+```
+Will return `Hello World!` value.
+
+**Writing output to a file:**
+```
+$ b64 -d SGVsbG8gV29ybGQh -o decoded.txt
+```
+
+Will create a file named `decoded.txt` containingext " *Hello world!* " text, instead of displaying the output in the console.
+
+**Reading input from a file:**
+```
+$ b64 -e -i decoded.txt
+```
+Will read file contents and encodode it, rather than using the command input.
 
 ##  :warning: Note
-This library is still under development breaking changes will occur, great features incoming :muscle:
+This library is still under development some changes may occur, great features incoming :muscle:
--- src/index.ts
+++ src/index.ts
@@ -2,25 +2,68 @@
 import { exit } from "process";
 import chalk from "chalk";
 import fs from "fs";
+import { program } from "commander";
+import figlet from 'figlet';
 
-console.log(chalk.red("Hello world!"));
 
-// gestion des paramètres manquants
-if (!process.argv[2] || !process.argv[3]) {
-  console.log("Usage:");
-  console.log("$ b64 [outputFileName] [base64EncodedData]");
+// display command header
+
+// define program parameters & options
+program
+.option('-d, --decode [data]', 'set mode to encoding')
+.option('-e, --encode [data]', 'set mode to decoding')
+.option('-i, --input-file <filename>', 'read input from a file')
+.option('-o, --output-file <filename>', 'write output to a file')
+program.parse();
+
+const options = program.opts();
+
+
+
+// if not arguments, we display help & exit the program
+if (!process.argv.slice(2).length) {
+  console.log(chalk.red(figlet.textSync("b64")));
+  console.log(chalk.green("base64-advanced-client"));
+  program.outputHelp();
   exit();
 }
 
-const fileName = process.argv[2];
-const base64Data = process.argv[3];
 
-// function to create file from base64 encoded string
-function base64ToFile(base64str: string, file: string) {
-  // create buffer object from base64 encoded string, it is important to tell the constructor that the string is base64 encoded
-  var buffer = Buffer.from(base64str, "base64");
-  // write buffer to file
-  fs.writeFileSync(file, buffer);
+// error handling
+if(options.encode && options.decode){
+  console.log(chalk.red('Encode & Decode flags can\'t be used at the same time.'));
+  exit();
 }
 
-base64ToFile(base64Data, fileName);
+let inputEncoding: BufferEncoding;
+let outputEncoding: BufferEncoding = 'utf8' as BufferEncoding;
+let inputBuffer: Buffer = Buffer.from('');
+
+// handle "encode" input
+if(options.encode){
+  inputEncoding = 'utf8';
+  outputEncoding = 'base64'
+  if(options.encode !== true){
+    inputBuffer = Buffer.from(options.encode, inputEncoding);
+  } else if(options.inputFile) {
+    inputBuffer = fs.readFileSync(options.inputFile);
+  }
+}
+
+//handle "decode" input
+if(options.decode){
+  inputEncoding = 'base64';
+  outputEncoding = 'utf8'
+  if(options.decode !== true){
+    inputBuffer = Buffer.from(options.decode, inputEncoding)
+  } else if(options.inputFile) {
+    inputBuffer = fs.readFileSync(options.inputFile);
+  }
+}
+
+// handle output
+if(options.outputFile){
+  fs.writeFileSync(options.outputFile, inputBuffer);
+} else {
+  console.log(chalk.yellow(inputBuffer.toString(outputEncoding)));
+}
