eee7b52eee06a5927ae1cac197d1867c5a8c2367
Acuao <acuao@live.fr>
2022-10-29 22:26:35 +0200

    added some tests

--- test/test.ts
+++ test/test.ts
@@ -6,6 +6,10 @@ import { getFileSha256 } from './test-utils.js';
 
 let errorCount = 0;
 
+
+
+
+
 console.log(chalk.blue('simple encoding', ':'));                                             
 if(execSync('b64 -e test').toString('utf-8') === 'dGVzdA==\n'){
   console.log(chalk.green('TEST Success !'));                                             
@@ -27,8 +31,55 @@ if(execSync('b64 -d dGVzdA==').toString('utf-8') === 'test\n'){
 
 
 
+console.log(chalk.blue('input file encoding', ':'));                                             
+if(execSync('b64 -e -i test/in-love-small.png').toString('utf-8') === 'iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAACXBIWXMAAAsTAAALEwEAmpwYAAABq0lEQVQ4jWNgQAP/D/AE/d/PfQGIfwDxfyj+8X8f9/n/+3kC0NVDNDEwML7NNJH9v4/zyv+93P//bhP4j6QZjP9u5///fy8PkM116XWSldR/hnomqOZQ5jvaCbvvaCT/fxHl9v+xa9B/EPtNnjVc87tSs/93NJP/P3IM/f8y3hksf0cr8fgVrVA2hlsaKRZgATR8VzsRYuM+7v93dRMw5MFq1BJtGL5OU027o5mEIfnQNhzugod24ZgGAPV8maiRwgAKsNdZthgKvvSpww340q+OIf860w7kurNAA7h+/NvD+/9FrAvEWVqJ/z/UGmIEIkgM5C1wWMW4/gfpARrwHWTAP5iir5PU/v+YL4ehGYZ/LpEGuwYuto/7HwOIwKZYS9X/v5RUCBhrq/njMvQf2AvYJOVlg/6rSYeCsYJcEHYDwF4ApzBMyR5nz/+3gP69CcQ9Lh64XHCGAZQ8sUn+2MH/f3e8LRiD2FgN2MPrC0mNwOSJK+Dw4AuIvLCKgQdoyFuiNe8Dqj3DwIWaoSCGXCTGZgzNKAbt4/EHpTBQCEPSCBCD2dxn4H5GAgAOzEfknjgxDwAAAABJRU5ErkJggg==\n'){
+  console.log(chalk.green('TEST Success !'));                                             
+} else {
+  console.log(chalk.red('TEST Failed !')); 
+  errorCount ++;                                             
+}
+
+
+
+
+console.log(chalk.blue('input file decoding', ':'));   
+if(execSync('b64 -d -i test/hello-world.txt.b64').toString('utf-8') === 'Hello World!\n'){
+  console.log(chalk.green('TEST Success !'));                                             
+} else {
+  console.log(chalk.red('TEST Failed !')); 
+  errorCount ++;                                             
+}                                          
+
+
+
+
+console.log(chalk.blue('decode to file', ':'));      
+execSync('b64 -d iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAACXBIWXMAAAsTAAALEwEAmpwYAAABq0lEQVQ4jWNgQAP/D/AE/d/PfQGIfwDxfyj+8X8f9/n/+3kC0NVDNDEwML7NNJH9v4/zyv+93P//bhP4j6QZjP9u5///fy8PkM116XWSldR/hnomqOZQ5jvaCbvvaCT/fxHl9v+xa9B/EPtNnjVc87tSs/93NJP/P3IM/f8y3hksf0cr8fgVrVA2hlsaKRZgATR8VzsRYuM+7v93dRMw5MFq1BJtGL5OU027o5mEIfnQNhzugod24ZgGAPV8maiRwgAKsNdZthgKvvSpww340q+OIf860w7kurNAA7h+/NvD+/9FrAvEWVqJ/z/UGmIEIkgM5C1wWMW4/gfpARrwHWTAP5iir5PU/v+YL4ehGYZ/LpEGuwYuto/7HwOIwKZYS9X/v5RUCBhrq/njMvQf2AvYJOVlg/6rSYeCsYJcEHYDwF4ApzBMyR5nz/+3gP69CcQ9Lh64XHCGAZQ8sUn+2MH/f3e8LRiD2FgN2MPrC0mNwOSJK+Dw4AuIvLCKgQdoyFuiNe8Dqj3DwIWaoSCGXCTGZgzNKAbt4/EHpTBQCEPSCBCD2dxn4H5GAgAOzEfknjgxDwAAAABJRU5ErkJggg== -o test/decodedImage.tmp.png');
+
+if( getFileSha256('test/decodedImage.tmp.png') ===  '40590e28491ad53b9acdb71697ea2977336819f8e170ad95451728d94e2a4a12'){
+  console.log(chalk.green('TEST Success !'));                                             
+} else {
+  console.log(chalk.red('TEST Failed !')); 
+  errorCount ++;                                             
+}
+fs.rmSync('test/decodedImage.tmp.png');
+
+
+
+console.log(chalk.blue('encode to file', ':'));                                             
+execSync('b64 -e "Hello World" -o test/encoded.tmp.txt.b64');
+
+if( getFileSha256('test/encoded.tmp.txt.b64') ===  'd9de98033e40166f4ba06a3d3b7fc34f7420242bbb9430a8584b2f3aaf8f9642'){
+  console.log(chalk.green('TEST Success !'));                                             
+} else {
+  console.log(chalk.red('TEST Failed !')); 
+  errorCount ++;                                             
+}
+fs.rmSync('test/encoded.tmp.txt.b64');
+
+
+
 console.log(chalk.blue('JWT decoding', ':'));      
-//OK//if(execSync('b64 --jwt eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiSm9obiBEb2UiLCJpYXQiOjE1MTYyMzkwMjJ9.hqWGSaFpvbrXkOWc6lrnffhNWR19W_S1YKFBx2arWBk').toString('base64') == 'SGVhZGVyIDogewogICAgImFsZyI6ICJIUzI1NiIsCiAgICAidHlwIjogIkpXVCIKfQpCb2R5IDogewogICAgIm5hbWUiOiAiSm9obiBEb2UiLAogICAgImlhdCI6IDE1MTYyMzkwMjIKfQo='){
 if(execSync('b64 --jwt eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiSm9obiBEb2UiLCJpYXQiOjE1MTYyMzkwMjJ9.hqWGSaFpvbrXkOWc6lrnffhNWR19W_S1YKFBx2arWBk').toString('utf8') === fs.readFileSync('./test/jwt-output.txt').toString('utf8')){
   console.log(chalk.green('TEST Success !'));                                             
 } else {
@@ -39,17 +90,37 @@ if(execSync('b64 --jwt eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiSm9obiBE
 
 
 
-console.log(chalk.blue('base 64 from file to file', ':'));      
-//OK//if(execSync('b64 --jwt eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiSm9obiBEb2UiLCJpYXQiOjE1MTYyMzkwMjJ9.hqWGSaFpvbrXkOWc6lrnffhNWR19W_S1YKFBx2arWBk').toString('base64') == 'SGVhZGVyIDogewogICAgImFsZyI6ICJIUzI1NiIsCiAgICAidHlwIjogIkpXVCIKfQpCb2R5IDogewogICAgIm5hbWUiOiAiSm9obiBEb2UiLAogICAgImlhdCI6IDE1MTYyMzkwMjIKfQo='){
-execSync('b64 -d -i test/bus.gif.b64 -o test/bus.tmp.gif');
+console.log(chalk.blue('decode from file to file', ':'));      
+execSync('b64 -d -i test/bus.gif.b64 -o test/decoded.tmp.gif');
 
-if( getFileSha256('test/bus.tmp.gif') ===  '396c7b535869e4418fd9d9657635e5134c6ba626819059f0b26e9b8fbc82a761'){
+if( getFileSha256('test/decoded.tmp.gif') ===  '396c7b535869e4418fd9d9657635e5134c6ba626819059f0b26e9b8fbc82a761'){
   console.log(chalk.green('TEST Success !'));                                             
 } else {
   console.log(chalk.red('TEST Failed !')); 
   errorCount ++;                                             
 }
-fs.rmSync('test/bus.tmp.gif');
+fs.rmSync('test/decoded.tmp.gif');
+
+
+
+console.log(chalk.blue('encode from file to file', ':'));      
+execSync('b64 -e -i test/bus.gif -o test/encoded.tmp.gif');
+
+if( getFileSha256('test/encoded.tmp.gif') ===  'ffe3bbb08cd54b7c14d491d1f82ec7b17d88b06c097ead6d76c67faf014835e8'){
+  console.log(chalk.green('TEST Success !'));                                             
+} else {
+  console.log(chalk.red('TEST Failed !')); 
+  errorCount ++;                                             
+}
+fs.rmSync('test/encoded.tmp.gif');
+
+
+
+
+
+
+
+
 
 
 
